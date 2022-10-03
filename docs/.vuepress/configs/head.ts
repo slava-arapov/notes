e1fdba6998235d60e2bd2d9de7d6972920a08d1f
Slava Arapov <sla.ckf@gmail.com>
2022-10-03 15:54:35 +0300

    Yandex.Metrika counter added

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -37,4 +37,6 @@ export const head: HeadConfig[] = [
   ],
   ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  ['script', { type: 'text/javascript'}, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(90807140, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });'],
+  ['noscript', { }, '<div><img src="https://mc.yandex.ru/watch/90807140" style="position:absolute; left:-9999px;" alt="" /></div>']
 ]
